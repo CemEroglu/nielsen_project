c794b84d6a650e6b9e016da811d2d658eb2bbf86
TURKCELL\tcceroglu <cem.eroglu@turkcell.com.tr>
2021-06-21 14:35:13 +0300

    Data evaluation applied.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { useState, useEffect } from 'react'
 import * as Services from './services/HackernewsAPIs'
 import LineChart from './components/LineChart';
-import {PulseLoader} from "react-spinners";
+import { PulseLoader } from "react-spinners";
 
 const App = () => {
   const [amount, setAmount] = useState(10);
@@ -17,14 +17,30 @@ const App = () => {
   }, [])
 
   const setDetails = (data) => {
-    setIsLoading(true)
+    let combinedData = [];
     let descendantsArray = [];
     let scoresArray = [];
     data.map((item, index) => {
       Services.getElementsDetails(item).then(res => {
-        descendantsArray.push(res.descendants)
-        scoresArray.push(res.score)
+        if (res.descendants == "Unknown" || res.score == "Unknown") {
+          return
+        }
+        combinedData.push(
+          {
+            "ID": item,
+            "descendant": res.descendants,
+            "score": res.score
+          }
+        )
         if (index == data.length - 1) {
+          combinedData.sort(function (a, b) {
+            return a.descendant - b.descendant;
+          });
+
+          combinedData.map(item => {
+            descendantsArray.push(item.descendant)
+            scoresArray.push(item.score)
+          })
           setDescendants(descendantsArray)
           setScores(scoresArray)
           setIsLoading(false)
@@ -34,6 +50,7 @@ const App = () => {
   }
 
   const whenAmountChange = (event) => {
+    setIsLoading(true)
     setAmount(event.target.value)
     Services.getSomeElements(event.target.value).then(data => setDetails(data))
   }
@@ -47,11 +64,11 @@ const App = () => {
         <option>40</option>
         <option>50</option>
       </select>
-      {isLoading ? (<PulseLoader size={40} color="turquoise" loading/>) 
-      : (scores.length > 0 ? (<LineChart title="Score Of Descendants" descendants={descendants} scores={scores}></LineChart>) 
-        : "")}
+      {isLoading ? (<PulseLoader size={40} color="turquoise" loading />)
+        : (scores.length > 0 ? (<LineChart title="Score Of Descendants" descendants={descendants} scores={scores}></LineChart>)
+          : "")}
 
-      
+
     </div>
   );
 }
